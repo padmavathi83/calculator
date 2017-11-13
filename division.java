3d0a1543aca87f1cfe71a7726eac0648150c2c96
sai <saivenkateshbellamkonda@gmail.com>
2017-11-13 16:03:27 +0530

    Modified division to divide by zero error

--- division.java
+++ division.java
@@ -2,6 +2,14 @@ public class division
 {
 public void div(int a,int b)
 {
+if(b!=0)
+{
 System.out.println("Division of two numbers is : "+(a/b)+"\n");
+}
+else
+{
+System.out.println("Second value must be NON-ZERO");
+}
+
 }
 }
