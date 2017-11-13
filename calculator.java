33cf9c62a9dc334e2ecbab60bbe584a4aa39d987
paddu <padmavathi83vattikonda@gmail.com>
2017-11-13 16:17:25 +0530

    Added Multiplication

--- calculator.java
+++ calculator.java
@@ -9,7 +9,8 @@ Scanner s=new Scanner(System.in);
 addition add=new addition();
 subtraction sub=new subtraction();
 division div=new division();
-System.out.println("Enter an option:\n 1.Addition \n2.Subtraction\n3.Division\n");
+multiplication mul=new multiplication();
+System.out.println("Enter an option:\n 1.Addition \n2.Subtraction\n3.Division\n4.Multiplication");
 int c=s.nextInt();
 System.out.println("Enter first value:\n");
 a=s.nextInt();
@@ -21,6 +22,8 @@ else if(c==2)
 sub.sub(a,b);
 else if(c==3)
 div.div(a,b);
+else if(c==4)
+mul.mul(a,b);
 else
 System.out.println("Enter correct option");
 }
