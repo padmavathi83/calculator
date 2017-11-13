fbbe5f0cb0002248a62df57b7caf498cb105695d
paddu <padmavathi83vattikonda@gmail.com>
2017-11-13 15:33:09 +0530

    Added Subtraction

--- calculator.java
+++ calculator.java
@@ -4,13 +4,21 @@ public class calculator
 {
 public static void main(String[] args) 
 {
-int a,b;
+int a,b,ch;
 Scanner s=new Scanner(System.in);
 addition add=new addition();
-System.out.println("Enter first value:");
+subtraction sub=new subtraction();
+System.out.println("Enter an option:\n 1.Addition \n2.Subtraction\n");
+int c=s.nextInt();
+System.out.println("Enter first value:\n");
 a=s.nextInt();
-System.out.println("Enter Second value:");
+System.out.println("Enter Second value:\n");
 b=s.nextInt();
+if(c==1)
 add.add(a,b);
+else if(c==2)
+sub.sub(a,b);
+else
+System.out.println("Enter correct option");
 }
 }
