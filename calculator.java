11437e396c04a17ed70a5cbc78a6f2ac124188a6
sai <saivenkateshbellamkonda@gmail.com>
2017-11-13 16:26:49 +0530

    Modified

--- calculator.java
+++ calculator.java
@@ -10,21 +10,23 @@ addition add=new addition();
 subtraction sub=new subtraction();
 division div=new division();
 multiplication mul=new multiplication();
-System.out.println("Enter an option:\n 1.Addition \n2.Subtraction\n3.Division\n4.Multiplication");
-int c=s.nextInt();
+do
+{
+System.out.println("Enter an option:\n 1.Addition \n2.Subtraction\n3.Division\n4.Multiplication\n5.exit\n");
+ch=s.nextInt();
 System.out.println("Enter first value:\n");
 a=s.nextInt();
 System.out.println("Enter Second value:\n");
 b=s.nextInt();
-if(c==1)
-add.add(a,b);
-else if(c==2)
-sub.sub(a,b);
-else if(c==3)
-div.div(a,b);
-else if(c==4)
-mul.mul(a,b);
-else
-System.out.println("Enter correct option");
+switch(ch)
+{
+case1: add.add(a,b); break;
+case2: sub.sub(a,b); break;
+case3: div.div(a,b); break;
+case4: mul.mul(a,b); break;
+case5: System.exit(0); break;
+default: System.out.println("Enter correct option");
+}
+}while(ch!=5);
 }
 }
